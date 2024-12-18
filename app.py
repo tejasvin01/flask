e9ec3e754f944488a1af05f1bdf610ed256d32e2
Tejasvin R <123625068+tejasvin01@users.noreply.github.com>
2024-12-18 23:16:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -682,4 +682,4 @@ def add_volunteer():
     return render_template('add_volunteer.html')
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=7000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
